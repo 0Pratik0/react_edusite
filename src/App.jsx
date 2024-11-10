52292b2313fdd430bedf185fe86c535e49887ca2
Pratik <pratik.tamrakar45@gmail.com>
2024-11-10 21:17:15 +0545

    making segments responsible and completing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/programs";
@@ -8,7 +8,10 @@ import Campus from "./Components/Campus/Campus";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 const App = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -19,7 +22,7 @@ const App = () => {
         {/* in title we are passing the props subtitle and title */}
         <Title subTitle="Our Program" title="What We Offer" />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
         <Title subTitle="TESTIMONIALS" title="What Students Says" />
@@ -28,6 +31,7 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import './About.css'
-import about_img from '../../assets/about.png'
-import play_icon from '../../assets/play-icon.png'
-const About = () => {
+import React from "react";
+import "./About.css";
+import about_img from "../../assets/about.png";
+import play_icon from "../../assets/play-icon.png";
+const About = ({ setPlayState }) => {
   return (
     <div className="about">
-        <div className="about-left">
-            <img src={about_img} alt="" className='about-img'/>
-            <img src={play_icon} alt="" className='play-icon'/>
-        </div>
-        <div className="about-right">
-            <h3>ABOUT UNIVERSITY</h3>
-            <h2>Nurturing Tomorrow's Leaders Today</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos quos obcaecati quo perferendis ducimus minus, ullam explicabo autem unde accusamus laudantium magnam, molestiae alias quibusdam qui nobis! Quos, voluptatem cum.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi delectus deserunt eius dolorum a laborum asperiores repudiandae labore unde et, sint voluptatem sapiente corporis nobis quo sit accusantium, magni optio!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia porro ex, temporibus eum architecto facere, itaque, natus expedita excepturi molestiae similique. Atque, officiis sapiente nulla sequi quisquam iusto incidunt fugiat?</p>
-        </div>
+      <div className="about-left">
+        <img src={about_img} alt="" className="about-img" />
+        <img
+          src={play_icon}
+          alt=""
+          className="play-icon"
+          onClick={() => {
+            setPlayState(true);
+          }}
+        />
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos quos
+          obcaecati quo perferendis ducimus minus, ullam explicabo autem unde
+          accusamus laudantium magnam, molestiae alias quibusdam qui nobis!
+          Quos, voluptatem cum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          delectus deserunt eius dolorum a laborum asperiores repudiandae labore
+          unde et, sint voluptatem sapiente corporis nobis quo sit accusantium,
+          magni optio!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia porro
+          ex, temporibus eum architecto facere, itaque, natus expedita excepturi
+          molestiae similique. Atque, officiis sapiente nulla sequi quisquam
+          iusto incidunt fugiat?
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
