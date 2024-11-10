8aa64fdf4fdb85c2d38bbc55827c5414ef1e027c
Pratik <pratik.tamrakar45@gmail.com>
2024-11-10 16:47:31 +0545

    adding a footer and functionality to some buttons and forms

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
 
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+import "./Contact.css";
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "d463efc9-3e60-45a8-8f2f-23d7eae17c00");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div className="contact">
       <div className="contact-col">
-        <h3>Send us a message</h3>
+        <h3>
+          Send us a message
+          <img src={msg_icon} alt=" " />
+        </h3>
         <p>
           Feel free to reach out through contact form or find our contact
           information below. Your feedback, questions, and suggestions are
@@ -12,12 +45,50 @@ const Contact = () => {
           university community.
         </p>
         <ul>
-          <li>pratik.tamrakar45@gmail.com</li>
-          <li>+977 9869185262</li>
-          <li>Lagankhel, Lalitpur, Nepal</li>
+          <li>
+            <img src={mail_icon} alt="" />
+            pratik.tamrakar45@gmail.com
+          </li>
+          <li>
+            <img src={phone_icon} alt="" />
+            +977 9869185262
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            Lagankhel, Lalitpur, Nepal
+          </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter you name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter you mobile number"
+            required
+          />
+          <label>Write your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now
+            <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   );
 };
