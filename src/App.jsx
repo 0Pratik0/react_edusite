f97b1c242a10bb22a6aa39d89665a5f8dd23d06f
Pratik <pratik.tamrakar45@gmail.com>
2024-11-10 15:06:39 +0545

    making about, contact, campus, hero and testimonial sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Title from "./components/Title/Title";
-import Programs from "./components/Programs/Programs";
-
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/programs";
+import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
 const App = () => {
   return (
     <div>
       <Navbar />
+
       <Hero />
+
       <div className="container">
         {/* in title we are passing the props subtitle and title */}
         <Title subTitle="Our Program" title="What We Offer" />
         <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Students Says" />
+        <Testimonials />
+        <Title subTitle="Contact Us" title="Get in Touch" />
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>Send us a message</h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>pratik.tamrakar45@gmail.com</li>
+          <li>+977 9869185262</li>
+          <li>Lagankhel, Lalitpur, Nepal</li>
+        </ul>
+      </div>
+      <div className="contact-col"></div>
+    </div>
+  );
+};
+
+export default Contact;
